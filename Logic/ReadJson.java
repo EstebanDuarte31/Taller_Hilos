7da32157ae96577be7148506fed271671d927812
EstebanDuarte31 <67130449+EstebanDuarte31@users.noreply.github.com>
2020-09-03 21:25:35 -0500

    Update ReadJson.java
    bugs arreglados

--- Logic/ReadJson.java
+++ Logic/ReadJson.java
@@ -5,11 +5,11 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.util.ArrayList;
 
-public class ReadJson  extends Thread{
-//    private Thread t;
+public class ReadJson extends Thread {
+    //    private Thread t;
     private String routJSON;
 
-     boolean state=true;
+    boolean state = true;
 
     Origen[] OriginsArray;
 
@@ -22,55 +22,51 @@ public class ReadJson  extends Thread{
         arrayListDestination = new ArrayList<ArrayList<Node<Destination>>>();
 
     }
-
+//metodo sobrecargado
     @Override
     public void run() {
-            // Creo una instancia de GSON
-            Gson gson = new Gson();
-
-            // Leo el fichero
-            String fichero = "";
-
-            try (BufferedReader br = new BufferedReader(new FileReader(routJSON))) {
-                String linea;
-                while ((linea = br.readLine()) != null) {
-                    fichero += linea;
-                }
-
-            } catch (FileNotFoundException ex) {
-                System.out.println(ex.getMessage());
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-
-
-            OriginsArray = gson.fromJson(fichero, Origen[].class);
-            createNodes();
+        readJSOn();
     }
 
+    //metodo que lee el archivo .json
+    public void readJSOn() {
+        // Creo una instancia de GSON
+        Gson gson = new Gson();
 
+        // Leo el fichero
+        String fichero = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(routJSON))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                fichero += linea;
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        OriginsArray = gson.fromJson(fichero, Origen[].class);
+        createNodes();
+    }
 
     public void createNodes() {
 
-        for(Origen user : OriginsArray) {
+        for (Origen user : OriginsArray) {
             Node<Origen> origenNode = new Node<Origen>(new Origen(user.getSource()), user.getSource());
             arrayListOrigen.add(origenNode);
 
 
-
             ArrayList<Node<Destination>> arrayListDestination = new ArrayList<Node<Destination>>();
             for (int i = 0; i < user.getTargets().size(); i++) {
-                Node<Destination> destinoNode = new Node<Destination>(new Destination(user.getTargets().get(i).getTarget(), user.getTargets().get(i).getDistance(), user.getTargets().get(i).getValue()), user.getTargets().get(i).getTarget().toString());
+                Node<Destination> destinoNode = new Node<Destination>(new Destination(user.getTargets().get(i).getTarget(), user.getTargets().get(i).getDistance(), user.getTargets().get(i).getValue()), user.getTargets().get(i).getTarget());
                 arrayListDestination.add(destinoNode);
 //            graph.addEdge(new Distance(or.getTargets().get(i).getDistance(), origenNode.getId()+arrayListDestination.get(i).getId()), origenNode, arrayListDestination.get(i), EdgeType.DIRECTED);
             }
             this.arrayListDestination.add(arrayListDestination);
         }
-
-        System.out.println(arrayListOrigen);
-
-
-
+//        System.out.println("REadSon" + arrayListOrigen);
     }
 
     public ArrayList<Node<Origen>> getArrayListOrigen() {
